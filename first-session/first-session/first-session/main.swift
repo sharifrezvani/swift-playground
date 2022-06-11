eb285933591a8d184cd7ecec757738273a5d6717
Macvps <macvps@static.21.222.251.148.clients.your-server.de>
2022-06-11 05:07:54 -0700

    studied day 1

---
+++ first-session/first-session/first-session/main.swift
@@ -0,0 +1,115 @@
+//
+//  main.swift
+//  first-session
+//
+//  Created by Macvps on 6/11/22.
+//
+
+import Foundation
+import Cocoa
+
+// variables and constants ------------------------------
+
+
+
+
+
+//var greeting = "Hello, playground"
+//
+//var name = "Ted"
+//name = "Rebecca"
+//name = "Keeley"
+//
+//let character = "Daphne"
+//This is wrong -> character = "Eloise"
+//This is wrong -> character = "Francesca"
+//
+//var playerName = "Roy"
+//print(playerName)
+//
+//playerName = "Dani"
+//print(playerName)
+//
+//playerName = "Sam"
+//print(playerName)
+//
+//let managerName = "Micheal Scott"
+//let dogBreed = "Samoyed"
+//let meaningOFLife = "How many roads must a man walk down?"
+
+// creating strings ------------------------------------------
+
+
+//let actor = "Denzel Washington"
+//let fileName = "paris.jpg"
+//let result = "⭐ You win! ⭐"
+//let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+//
+//let movie = """
+//A day in
+//the life of an
+//Apple Engineer
+//"""
+//
+//let nameLength = actor.count
+//print(nameLength)
+//
+//print(result.uppercased())
+//
+//print(movie.hasPrefix("A day"))
+//
+//print(fileName.hasSuffix(".jpg"))
+
+// storing whole numbers -------------------------------------------
+
+//let score = 10
+//let reallyBig = 100_000_000
+//
+//let lowerScore = score - 2
+//let higherScore = score + 10
+//let doubledScore = score * 2
+//let squaredScore = score * score
+//let halvedScore = score / 2
+//
+//
+//var counter = 10
+//counter += 5 // (which is counter = counter + 5)
+//print(counter)
+//
+//counter *= 2
+//print(counter)
+//counter -= 10
+//print(counter)
+//counter /= 2
+//print(counter)
+//
+//let number = 120
+//print(number.isMultiple(of: 3))
+//print(90.isMultiple(of: 3))
+
+
+// storing decimal numbers -------------------------------------------
+
+
+
+let number = 0.1 + 0.2
+print(number)
+
+let a = 1
+let b = 2.0 // (if there's a point, then swift treats it as a double)
+let c = a + Int(b) // (or let c = Double(a) + b)
+
+let firstDouble = 3.1
+let secondDouble = 3131.3131
+let thirdDouble = 3.0
+
+let firstInt = 3
+
+var name = "Nicholas Cage"
+name = "John Travolta"
+// name = 57 // (this is not allowed)
+var rating = 5.0
+rating *= 2 // (or 2.0)
+
+
+
