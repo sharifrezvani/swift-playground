12bb29dc5d3146efa3abcd43cdbff797e2c812d1
Macvps <macvps@static.21.222.251.148.clients.your-server.de>
2022-06-11 06:46:29 -0700

    studied day 2

---
+++ second-session/second-session/second-session/main.swift
@@ -0,0 +1,72 @@
+//
+//  main.swift
+//  second-session
+//
+//  Created by Macvps on 6/11/22.
+//
+
+import Foundation
+import Cocoa
+
+// storing truth with booleans ------------------------
+
+
+
+//let fileName = "paris.jpg"
+//print(fileName.hasSuffix(".jpg"))
+//
+//let number = 120
+//print(number.isMultiple(of: 3))
+//
+//let goodDogs = true
+//var gameOver = false
+//print(gameOver)
+//gameOver.toggle()
+//print(gameOver)
+//
+//
+//let isMultiple = 120.isMultiple(of: 3)
+//
+//var isAuthenticated = false
+//print(isAuthenticated)
+//isAuthenticated = !isAuthenticated
+//print(isAuthenticated)
+
+
+// joining strings ----------------------------------
+
+
+
+//let firstPart = "Hello, "
+//let secondPart = "world!"
+//let greeting = firstPart + secondPart
+//
+//let people = "Haters"
+//let action = "hate"
+//let lyric = people + " gonna " + action
+//
+//let luggageCode = "1" + "2" + "3" + "4" + "5" // (swift adds these one by one. like: "12" + "3" + "4" + "5" and so on)
+//
+//let name = "Taylor"
+//let age = 26
+//let message = "Hello, my name is \(name) and I'm \(age) years old." //(this is much more efficient, and it let's you add different types of data into a string.)
+//print(message)
+//
+//let number = 11
+//let missionMessage = "Apollo " + number + " landed on the moon." // (this is invalid, unless you say String(number))
+//
+//print("5 x 5 is \(5 * 5).")
+
+
+// checkpoint 1 --=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
+
+let celsiusTemperature = 13.0
+let fahrenheitTemperature = (Int(celsiusTemperature) * 9 / 5) + 32
+
+print("\(celsiusTemperature) degrees celsius is \(fahrenheitTemperature) degrees fahrenheit.")
+
+
+
+
+
+
